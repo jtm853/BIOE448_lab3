eee69f82456ddc85aa5354d6241e358d724570b1
jtm853 <johnny469225@gmail.com>
2025-01-29 14:42:10 -0600

    Added setup code for LCD

--- test/test.ino
+++ test/test.ino
@@ -3,10 +3,14 @@ Liquid Crystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // put your setup code here, to run once:
+  lcd.begin(16, 2);
+  lcd.print("My juicer is: ")
+  lcd.setCursor(0, 1);
+  lcd.print("Large!")
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(50)
 }
