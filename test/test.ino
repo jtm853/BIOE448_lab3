9e53ee57ffbd74cbc62114531eed301f3c67ce0f
jtm853 <johnny469225@gmail.com>
2025-02-10 11:27:54 -0600

    Updated names to be displayed on LCD

--- test/test.ino
+++ test/test.ino
@@ -4,9 +4,9 @@ Liquid Crystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16, 2);
-  lcd.print("My juicer is: ")
+  lcd.print("Jonathan")
   lcd.setCursor(0, 1);
-  lcd.print("Large!")
+  lcd.print("Akshay")
 
 }
 
